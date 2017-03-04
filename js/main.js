099d6a6865f35a5d7e565ffdbd20fcd2479c836d
Marisol Erika <keiragroban@gmail.com>
2017-03-04 13:04:14 -0500

    edit catgame html

--- js/main.js
+++ js/main.js
@@ -1,89 +1,74 @@
-$(document).ready(function(){
-  var nombre1 = $('#name1').val();
-  console.log(nombre1);
-  var nombre2 = $('#name2').val();
-  console.log(nombre2);
-  var player=1;
-  var table = $('table');
-  var result = $('.result');
-  var turn = $('.turn');
-  nextGame(turn, player);
-  $('td').click(function() {
-    td=$(this);
-    var status = is_full(td);
-    if(!status) {
-      var icon = iconPlayer(player);
-      setNewStatus(td, icon);
-      if(playWinner(table, icon)) {
-        result.html('<img src="css/catdance.gif" alt="Ganaste!!"> </br> Ganó el jugador número ' + player);
-        turn.html("");
-      } else {
-        player = setNextGame(player);
-        nextGame(turn, player);
-      }
-    } else {
-      false;
-    }
-  });
-  
-  $('.reset').click(function() {
-    player = 1;
-    result.html('');
-    reset(table);
-    nextGame(turn, player);
-  }); 
+$(document).ready(function(){  
+var round="x";
+var player1=[]; 
+var player2=[];  
+var win1 = [1,5,9];
+var win2 = [1,4,7];
+var win3 = [1,2,3];
+var win4 = [4,5,6];     
+var win5 = [7,8,9];
+var win6 = [2,5,8];
+var win7 = [3,6,9];
+var win8 = [3,5,7];
+var wins = [win1, win2, win3, win4, win5, win6, win7, win8];
+var score1 = 0;
+var score2=0;
+var startGame=function(){
+ player1 = [];
+ player2 = [];
+$(".square").each(function(){
+$(this).text("");
 });
-
-function is_full(td) {
-  if(td.hasClass('iconX') || td.hasClass('iconO')) {
-    return 1;
-  } else {
-    return false;
+wins=[win1, win2, win3, win4, win5, win6, win7, win8];
+round="x";
+}
+$(".clickable").click(function(){
+var celltext=$(this).text();
+if(celltext==""){
+  if(round=="x"){
+      $(this).text("x");
+      player1.push($(this).data("val"));
+      round="o";
   }
-}
-function setNewStatus(td, icon) {
-  return td.addClass(icon);
-}
-function iconPlayer(player) {
-  if(player == 1) {
-    return 'iconX';
-  } else {
-    return 'iconO';
-  }
-}
-function setNextGame(player) {
-  if(player == 1) {
-    return player = 2;
-  } else {
-    return player = 1;
-  }
-}
-function nextGame(turn, player) {
-  turn.html('Turno para jugador : '+ player);
-}
-function playWinner(table, icon) {
-  var winner = 0;
-  if(table.find('.square1').hasClass(icon) && table.find('.square2').hasClass(icon) && table.find('.square3').hasClass(icon)) {
-    winner = 1;
-  } else if (table.find('.square1').hasClass(icon) && table.find('.square4').hasClass(icon) && table.find('.square7').hasClass(icon)) {
-    winner = 1;
-  } else if (table.find('.square1').hasClass(icon) && table.find('.square5').hasClass(icon) && table.find('.square9').hasClass(icon)) {
-    winner = 1;
-  } else if (table.find('.square4').hasClass(icon) && table.find('.square5').hasClass(icon) && table.find('.square6').hasClass(icon)) {
-    winner = 1;
-  } else if (table.find('.square7').hasClass(icon) && table.find('.square8').hasClass(icon) && table.find('.square9').hasClass(icon)) {
-    winner = 1;
-  } else if (table.find('.square2').hasClass(icon) && table.find('.square5').hasClass(icon) && table.find('.square8').hasClass(icon)) {
-    winner = 1;
-  } else if (table.find('.square3').hasClass(icon) && table.find('.square6').hasClass(icon) && table.find('.square9').hasClass(icon)) {
-    winner = 1;
-  } else if (table.find('.square3').hasClass(icon) && table.find('.square5').hasClass(icon) && table.find('.square7').hasClass(icon)) {
-    winner = 1;
-  }
-  return winner;
-}
-function reset(table) {
-  table.find('td').each(function() {
-    $(this).removeClass('iconO').removeClass('iconX');
-  });
-}
+  else{
+      $(this).text("o");
+      player2.push($(this).data("val"));
+       round="x"; 
+      }
+      $.each(wins, function(i,v){
+        if(player1.length>=3){
+          var flag = true;
+          $.each(v,function(key,value){
+              if($.inArray(value,player1)==-1){
+                flag = false;
+              }
+          })
+          if(flag == true){
+            alert("player1 win!");
+             score1++ ;
+            $("#score1").text("X: SCORE="+score1);    
+            return;                                                     
+          }
+        }
+        if(player2.length>=3){
+          var flag = 1;
+          $.each(v,function(key,value){
+              if($.inArray(value,player2)==-1){
+                flag = 2;
+         }
+        })
+          if(flag == 1){
+            alert("player2 win!");
+             score2++ ;                  
+            $("#score2").text("O: SCORE="+score2);
+            return;  
+          }
+        }
+     });
+  };
+});
+$("#startagain").click(function(){
+            startGame();                    
+});
+});
+//result.html('<img src="css/catdance.gif" alt="Ganaste!!"> </br> Ganó el jugador número ' + player);
