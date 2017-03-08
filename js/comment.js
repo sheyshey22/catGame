b2b1d9fc90014de89daf2b864be6f51e7404aa32
sheyshey22 <you@example.com>
2017-03-08 05:54:09 -0500

    ajax

--- js/comment.js
+++ js/comment.js
@@ -1,40 +1,21 @@
-$(document).ready(function(){
-    var comments=$('comments');
-    $("#btn5").click(function(){
+$(document).ready(function() {
+
+    $("#btn5").click(function() {
         $.ajax({
-          type:'GET',
-          url: "http://test-ta.herokuapp.com/games/:game_id/comments", 
-          success: function(data){
-            console.log('success', data);
-        }});
+        type : 'GET',
+        url : 'http://test-ta.herokuapp.com/games/1/comments',
+        data : {tipo:'0'},
+        dataType : 'json',
+        success : function(data) {
+           console.log(JSON.stringify(data));
+           $.each(data, function(i,item) {
+            console.log(item);
+                update(data);
+            });
+        },
+            
     });
-});
-
-//
-/*/function addComment() {
-
-  var containerComentarios = document.getElementById('sendComment');
-  var nombrePersona = document.getElementById('name3').value;
-  var contenidoComentario = document.getElementById('boxComent').value;
-  var nuevoComentario = document.createElement('div');
-  var contenedorNombre = document.createElement('strong');
-  var contenedorPost = document.createElement('p');
-  var division = document.createElement('hr');
-  var nodoNombre = document.createTextNode(nombrePersona + ' dice:');
-  var nodoPosteo = document.createTextNode(contenidoComentario);
-
-
-  contenedorNombre.appendChild(nodoNombre);
-  contenedorPost.appendChild(nodoPosteo);
-
-  //agregando nuevos comentarios
-  nuevoComentario.appendChild(contenedorNombre);
-  nuevoComentario.appendChild(contenedorPost);
-  nuevoComentario.appendChild(division);
-  nuevoComentario.setAttribute('class', 'well');
-  containerComentarios.appendChild(nuevoComentario);
-
-  //reseteando campos
-  document.getElementById('name3').value = "";
-  document.getElementById('boxComment').value = "";
-}
+    });
+    function update(info)
+    {
+      $('#listComment').html('<div class="panels panel-white post panel-shadow"> <div class="post-heading"> <div class="pull-left image"> <img src="img/bob1.jpg" class="img-circle avatar" alt="user profile image"> </div> <div class="pull-left meta"> <div class="title h5"> <a id="name"><b>'+info[0].name+'</b></a> dice: </div> <h6 class="text-muted time">1 minute ago</h6> </div> </div> <div class="post-description"> <p>'+info[0].content+'</p> <div class="stats"> <a href="#" class="btn btn-default stat-item"> <i class="fa fa-thumbs-up icon"></i>2 </a> <a href="#" class="btn btn-default stat-item"> <i class="fa fa-thumbs-down icon"></i>12 </a> </div> </div> </div>'
